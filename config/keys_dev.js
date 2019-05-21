c37bb008405af311d1a935675deb12a9ced98d3f
toihocweb <buidinhnhat2030@gmail.com>
2019-05-22 06:59:17 +0700

    a

---
+++ config/keys_dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    "mongodb://admin:admin@mongodb-1940-0.cloudclusters.net:10009/dev-connector?authSource=admin",
+  secretOrKey: "secret"
+};
+
+// mongoURI: "mongodb://nhat:nhat123@ds133556.mlab.com:33556/mern-stack",
