d8ffbd969a4dc14fd02def6f85098083e8eb57cf
Dinh Nhat <bd1010@nkcasia.com>
2020-07-08 16:41:24 +0700

    #a

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import * as Sentry from '@sentry/react';
 
-Sentry.init({ dsn: "https://6d80a00786ab4170b56bc5ce2de621c2@o417587.ingest.sentry.io/5318266" });
+// Sentry.init({ dsn: "https://6d80a00786ab4170b56bc5ce2de621c2@o417587.ingest.sentry.io/5318266" });
 
 
 
